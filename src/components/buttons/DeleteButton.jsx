3dc52b05b81d3a5a975b9f5546f4720022a0cf00
muh-hizbe <muh.saferagic@gmail.com>
2023-05-08 11:45:20 +0700

    cypress: delete activity card

--- src/components/buttons/DeleteButton.jsx
+++ src/components/buttons/DeleteButton.jsx
@@ -28,19 +28,17 @@ export const DeleteButton = ({ onDelete, text, itemName, dataCy }) => {
     return (
         <>
             <Dialog.Root open={open} onOpenChange={setOpen} key={'confirm'}>
-                <Dialog.Trigger data-cy={dataCy}>
-                    <button
-                        onClick={(e) => e.stopPropagation()}
-                        className="hover:text-red-500"
-                    >
-                        <HiOutlineTrash />
-                    </button>
+                <Dialog.Trigger data-cy={dataCy}
+                    className="hover:text-red-500"
+                >
+                    <HiOutlineTrash />
                 </Dialog.Trigger>
 
                 <Dialog.Portal>
                     {/* <Dialog.Overlay className="bg-black/50 data-[state=open]:animate-overlayShow data-[state=closed]:animate-overlayHide fixed inset-0" /> */}
                     <Dialog.Content
                         data-cy="modal-delete"
+                        asChild
                         className="data-[state=open]:animate-contentShow data-[state=closed]:animate-contentHide fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[10px] bg-white p-[43px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
                     >
                         <div className="flex items-center justify-center">
